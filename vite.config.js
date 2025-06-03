3a673e94cbe0e11f6b44d7ab1aaa1ff69e28b0f5
Ramyro Corrêa Aquines <ramyrocorrea@gmail.com>
2025-06-03 10:28:53 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  base: '/final-project-chemicalprocesses/'
+  //base: '/final-project-chemicalprocesses/'
 };
 
 export default config;
