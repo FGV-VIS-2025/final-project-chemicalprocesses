1f24efe096ba2057a80204276c9b2617f9c56220
Ana Júlia Amaro <anajulia.smsap2@gmail.com>
2025-06-03 09:15:50 -0300

    fifth attempt to configure for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,13 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.env.NODE_ENV === 'development';
-// const repoName = 'final-project-chemicalprocesses';
 
 export default {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : `final-project-chemicalprocesses`,
+      base: dev ? '' : '/final-project-chemicalprocesses'
     }
-  },
-  preprocess: vitePreprocess()
+  }
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  base: '/final-project-chemicalprocesses/'
+};
+
+export default config;
